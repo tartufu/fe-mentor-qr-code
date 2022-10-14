f43fd0eb9ce4c63af7861fd1220e6616d8275e00
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2022-10-14 21:22:36 +0800

    MVP for Task achieved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import QrCodeCard from "./Components/QrCodeCard";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App" style={{ backgroundColor: "hsl(212, 45%, 89%)" }}>
+      <Container style={{ height: "100vh" }}>
+        <Row className="h-100 d-flex justify-content-center align-items-center">
+          <Col className="d-flex justify-content-center">
+            <QrCodeCard />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
